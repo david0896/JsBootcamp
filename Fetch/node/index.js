9588a0a5e02c671444028c8453141f3a8891dd85
david0896 <davidvasquez0896@gmail.com>
2022-01-19 10:04:19 -0400

    - 106. Obtener stream del payload y unir sus pedazos como un sólo string - segunda parte

--- Fetch/node/index.js
+++ Fetch/node/index.js
@@ -30,30 +30,42 @@ const server = http.createServer((req, res) => {
         //console.log(headers);
 
     //Obtener payload, en el caso de haber uno 
-     
+    const decoder = new stringDecoder("utf-8");
+    let buffer = ''; //acumulador/recolector
 
+    //ir acumulando la data cuando el request recibe un payload codificando los paquetes(stream) a string
+    req.on('data', (data)=>{
+        buffer += decoder.write(data);
+    });
+
+    //se finaliza la acumulacion de datos al dejar de llegar data finalizando el decoder
+    req.on('end', ()=>{
+        buffer += decoder.end();
+
+        //aseguramos que la respuesta se ejecute al finalizar request ya que end es asincrono 
+        switch(rutaLimpia){
+            case "ruta":
+                res.end("Estas en /ruta bienvenido");
+                break;        
+            case "oso":
+                res.end("Estas en /oso que es una ruta con nombre animal"); 
+                break;       
+            case "carro":
+                res.end("Estas en /carro bienvenido :D");   
+                break;
+            default:
+                res.end("Desconozco esta ruta x____x"); 
+                break;  
+        }
+    });
     //Enviar una respuesta dependiendo de la ruta
         /*if(rutaLimpia === "ruta"){
             res.end("Estas en /ruta bienvenido");        
         }else{
             res.end("Estas en ruta que desconosco");        
         } */
+});
 
-    switch(rutaLimpia){
-        case "ruta":
-            res.end("Estas en /ruta bienvenido");
-            break;        
-        case "oso":
-            res.end("Estas en /oso que es una ruta con nombre animal"); 
-            break;       
-        case "carro":
-            res.end("Estas en /carro bienvenido :D");   
-            break;
-        default:
-            res.end("Desconozco esta ruta x____x"); 
-            break;  
-    }
-})
 server.listen(5000, () =>{
-    console.log("Se inicio el servidor correctamente")
-})
+    console.log("Se inicio el servidor correctamente");
+});
