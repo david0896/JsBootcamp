0c4e8c1cf899dee126e7c9a2fcff5a587a5109a5
david0896 <davidvasquez0896@gmail.com>
2022-02-01 14:45:29 -0400

    111. Capturar indice de la mascota en la url y obtener una sola mascota de la API

--- Fetch/node/index.js
+++ Fetch/node/index.js
@@ -23,12 +23,23 @@ const enrutador = {
     },
     //handler mascotas 
     mascotas: {
+        //prueba postman LISTAR MASCOTAS y OPTENER UNA SOLA MASCOTA
         GET: (data, callback)=>{
+            //evaluamos que en la ruta exista un indice a buscar
+            if(data.indice){
+                //luego evaluamos que ese indice exista en nuetro array recursos
+                if(recursos.mascotas[data.indice]){
+                   return callback(200, recursos.mascotas[data.indice]);
+                }
+                return callback(404, {mesaje: `Mascota con indice ${data.indice} no encontrada`});
+
+            }
             //200 OK
             //La solicitud ha tenido éxito. El significado de un éxito varía 
             //dependiendo del método HTTP:
             callback(200, recursos.mascotas);
         },
+        //prueba postman AGREGAR MASCOTA
         POST: (data, callback)=>{
             //console.log('data desde HANDLER/POST', {data});
 
@@ -60,14 +71,14 @@ const server = http.createServer((req, res) => {
         //console.log(req);
     const urlActual = req.url;
     const urlParseada = url.parse(urlActual, true);
-        //console.log({urlActual, urlParseada});
+        console.log("urls: ", {urlActual, urlParseada});
 
     //obtener la ruta
     const rutaActualpath = urlParseada.pathname;
         //console.log({rutaActual});
 
     //ruta limpia 
-    const rutaLimpia = rutaActualpath.replace(/^\/+|\/+$/g, ""); //elimina los / del principio del final 
+    const rutaLimpia = rutaActualpath.replace(/^\/+|\/+$/g, ""); //elimina los / del principio y del final usando Regex 
         //console.log(rutaLimpia);
 
     //obtenemos el metodo HTTP
@@ -87,11 +98,13 @@ const server = http.createServer((req, res) => {
     let buffer = ''; //acumulador/recolector
 
     //ir acumulando la data cuando el request recibe un payload codificando los paquetes(stream) a string
+    //se ejecuta con el evento data de la peticion(req)
     req.on('data', (data)=>{
         buffer += decoder.write(data);
     });
 
     //se finaliza la acumulacion de datos al dejar de llegar data finalizando el decoder
+    //se ejecuta con el evento end de la peticion(req)
     req.on('end', ()=>{
         buffer += decoder.end();
 
@@ -100,9 +113,19 @@ const server = http.createServer((req, res) => {
         buffer = JSON.parse(buffer);
     }
 
-    //Organizar data del request
+    //evaluar ruta si trae una variable ( //ruta/:variable ) en este caso indice a buscar en el array recursos
+    //indexOf se encarga de encontrar dendro de un string o un array el valor que se le pasa como parametro, devolviendo su indice y -1 si no lo encuentra
+    if(rutaLimpia.indexOf('/') > -1){
+        //usamos var para que las variables creadas puedan ser usadas fuera de este hambito
+        console.log("rutaLimpia ", rutaLimpia);
+        console.log("rutaLimpia split ", rutaLimpia.split('/'));
+        var [rutaPrincipal, indice] = rutaLimpia.split('/');//split devuelve un array y lo guardamos usando destructuring
+    }
+
+    //Organizar data del request(peticion)
     const data = {
-        ruta: rutaLimpia,
+        indice,
+        ruta: rutaPrincipal || rutaLimpia,
         query,
         metodo,
         headers,
@@ -113,8 +136,8 @@ const server = http.createServer((req, res) => {
 
     //Elegir el manejador dependiendo de la ruta y asignarle la funcion que el enrutador tiene 
     let handler;
-    if(rutaLimpia && enrutador[rutaLimpia] && enrutador[rutaLimpia][metodo]){
-        handler = enrutador[rutaLimpia][metodo];
+    if(data.ruta && enrutador[data.ruta] && enrutador[data.ruta][metodo]){
+        handler = enrutador[data.ruta][metodo];
     }else{
         handler = enrutador.noEncontrado;
     }
